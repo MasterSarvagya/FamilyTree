6281f3d3c01e3b3129a51ca9c1f2994e040db5b3
Sarvagya Srivastava <sarvagyasrivastava108@gmail.com>
2023-08-27 23:48:55 +0530

    Fixed Height & Width

--- Draw.js
+++ Draw.js
@@ -1,7 +1,9 @@
+let winHeight = 1080;
+let winWidth = 1920;
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
-let cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 };
-let cameraZoom = 1;
+let cameraOffset = { x: winWidth/2, y: winHeight/2 };
+let cameraZoom = 0.875;
 let MAX_ZOOM = 5;
 let MIN_ZOOM = 0.1;
 let SCROLL_SENSITIVITY = 0.0005;
@@ -10,22 +12,33 @@ let lastZoom = cameraZoom;
 let isDragging = false;
 let dragStart = { x: 0, y: 0 };
 let characterList = [];
+let lineList = [];
 
 function draw() {
 	prepareCanvas();
-	ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
+	ctx.clearRect(0,0, winWidth, winHeight);
 	drawText("",0,0,20,"arial");
-	for(var i = 0; i < characterList.length; i++)
-		drawCharacterCard(characterList[i][0],characterList[i][1],characterList[i][2],characterList[i][3], characterList[i][4].trim());
+	for(let i = 0; i < lineList.length; i++){
+		ctx.beginPath();
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = "2";
+		ctx.moveTo(lineList[i][0],lineList[i][1]);
+		ctx.lineTo(lineList[i][2],lineList[i][3]);
+		ctx.stroke();
+	}
+
+	for(let i = 0; i < characterList.length; i++)
+		drawCharacterCard(characterList[i][0],characterList[i][1],characterList[i][2],characterList[i][3],characterList[i][4].trim());
+
 	requestAnimationFrame( draw );
 }
 
 function prepareCanvas() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	ctx.translate( window.innerWidth / 2, window.innerHeight / 2 );
+	canvas.width = winWidth;
+	canvas.height = winHeight;
+	ctx.translate( winWidth / 2, winHeight / 2 );
 	ctx.scale(cameraZoom, cameraZoom);
-	ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y );
+	ctx.translate( -winWidth / 2 + cameraOffset.x, -winHeight / 2 + cameraOffset.y );
 }
 
 function getEventLocation(e) {
@@ -51,6 +64,7 @@ function drawCharacterCard(x,y,gender,img_src = "",name = "") {
 		ctx.strokeStyle = "blue";
 		ctx.fillStyle = "#b7e2fc";
 		img.src = "images/male.png";
+
 	}
 	else {
 		ctx.strokeStyle = "#e9658d";
@@ -65,7 +79,7 @@ function drawCharacterCard(x,y,gender,img_src = "",name = "") {
 	ctx.roundRect(x, y, 150, 200, [25]);
 	ctx.fill();
 	ctx.drawImage(img, x + 25, y + 10, 100, 100);			
-	ctx.fillStyle = "white";
+	ctx.fillStyle = "#111111";
 	drawText(name.split("\n",2)[0],x+(75 - ctx.measureText(name.split("\n",2)[0]).width/2),y+150,20,"arial");
 	drawText(name.split("\n",2)[1],x+(75 - ctx.measureText(name.split("\n",2)[1]).width/2),y+180,20,"arial");
 	ctx.stroke();
@@ -75,7 +89,7 @@ function prepareCharacterList(root) {
 	prepareCanvas();
 	let ans = [];
 	let main_queue=[];
-	main_queue.push(root);
+	main_queue.push(root[0]);
 	let temp=[];
 	while (main_queue.length) {
 		let n = main_queue.length;
@@ -93,17 +107,41 @@ function prepareCharacterList(root) {
 		let sib = v.length;
 		let dist = 0;
 		let fact = 1;
+		let level_min = Number.MAX_VALUE;
+		let level_max = 0;
 		for (let x of v){
 			dist += 0.5;
-			characterList.push([fact * parseInt(dist) * (canvas.width/sib) - 75,-canvas.height/2 + 25 + level*250,x.gender,"",x.first_name + "\n" + x.last_name]);
+			let partner = root[1].find(m => m.id == x.partner_id)
+			if(partner === undefined)
+				partner = new Member();
+			coord = {
+				x1 : fact * parseInt(dist) * (canvas.width/sib) - 175,
+				x2 : fact * parseInt(dist) * (canvas.width/sib) - 175 + 200,
+				y1 : -canvas.height/2 + level*250,
+				y2 : -canvas.height/2 + level*250
+			}
+
+			characterList.push([coord.x1,coord.y1,x.gender,"",x.first_name + "\n" + x.last_name]);			
+			characterList.push([coord.x2,coord.y2,partner.gender,"",partner.first_name + "\n" + partner.last_name]);
+			
+			lineList.push([coord.x1 + 150, coord.y1 + 100, coord.x2, coord.y2 + 100]);
+			lineList.push([(coord.x1 + coord.x2 + 150)/2, coord.y1 - 25,(coord.x1 + coord.x2 + 150)/2, coord.y2 + 100]);
+			lineList.push([(coord.x1 + coord.x2 + 150)/2, coord.y1 + 225,(coord.x1 + coord.x2 + 150)/2, coord.y2 + 100]);
+
+			if(level_min > coord.x1)
+				level_min = coord.x1;
+			if (level_max < coord.x2 + 150)
+				level_max = coord.x2 + 150;
 			fact *= -1;
 		}
+		lineList.push([level_min + 175, coord.y1 - 25,level_max - 175, coord.y2 -25]);
 		level += 1;
 	}
 }
 
 function centerCanvas() {
-	cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 };
+	cameraOffset = { x: winWidth/2, y: winHeight/2 };
+	cameraZoom = 0.875;
 }
 
 function onPointerDown(e) {
--- Load.js
+++ Load.js
@@ -1,7 +1,7 @@
 class Member{
 	id = 0;
-	first_name = "";
-	last_name = "";
+	first_name = "NAHI";
+	last_name = "HAI";
 	gender = "";
 	partner_id = 0;
 	parent_id = 0;
@@ -14,6 +14,7 @@ class MemberTree {
 		let response = await fetch(url);
 		let text = await response.text();
 		var members = [];
+		// text = this.list;
 		var lines = text.split("\n");
 		var header = lines[0].split(",");
 		for(var i = 1; i < lines.length-1; i++){
@@ -28,6 +29,7 @@ class MemberTree {
 	}
 	async makeTree(url){
 		var members = await this.getData(url);
+		var orig = members;
 		members = members.filter(m => m.parent_id != "0");
 		var parent_child_map = {}
 		members.reduce((tot,m) => {
@@ -42,6 +44,19 @@ class MemberTree {
 				parent.child_members.push(m);
 			}
 		});
-		return this.root;
+		return [this.root,orig];
 	}
+	list = `id,first_name,last_name,gender,partner_id,parent_id
+1,Akbal Bahadur,Srivastava,M,2,-1
+2,Nirmala,Srivastava,F,1,0
+3,Pankaj,Srivastava,M,4,1
+4,Aparna,Srivastava,F,3,0
+5,Mohak,Srivastava,M,6,3
+6,Sindhuja,Srivastava,F,5,0
+7,Mayuri,Srivastava,F,8,3
+8,Mayank,Srivastava,M,7,0
+9,Sarvagya,Srivastava,M,0,3
+10,Srithik,Srivastava,M,0,5
+11,Dev,Srivastava,M,0,7
+`;
 }
